7cea4a1410d43993e88956d4ddb2f3b7461305a4
robola11 <34795971+robola11@users.noreply.github.com>
2024-05-31 12:36:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const cart =document.getElementById("cart");
+const modal =document.getElementById("modal");
+
+
+cart.addEventListener('click', (e)=>{
+
+   
+     modal.classList.toggle('hidden');
+   
+});
